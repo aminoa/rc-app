f07a3ea0abfea289ec270e77cbdf6c1f5073b0e6
Aneesh Maganti <28660350+aminoa@users.noreply.github.com>
2025-04-09 23:36:34 -0400

    added base db server;

---
+++ attempt-two/db-server-rc/server.py
@@ -0,0 +1,36 @@
+# Runs a server that is accessible on http://localhost:4000/. 
+# When your server receives a request on http://localhost:4000/set?somekey=somevalue 
+# it should store the passed key and value in memory. 
+# When it receives a request on http://localhost:4000/get?key=somekey 
+# it should return the value stored at somekey.
+
+# TODO: save data to a file, make it performant
+
+from flask import Flask, request
+
+app = Flask(__name__)
+db = {}
+
+@app.route('/set', methods=['GET'])
+def set():
+    if len(request.args) != 1:
+        return "Invalid Request: ", 400
+
+    key, value = list(request.args.items())[0]
+
+    if not key or not value:
+        return "Invalid Request: Both key and value must be provided.", 400
+
+    db[key] = value
+    return f"Stored {key} = {value}"
+
+@app.route('/get', methods=['GET'])
+def get():
+    key = request.args.get('key')
+
+    if key not in db:
+        return "Error: Key not found.", 404
+    return db[key]
+
+if __name__ == '__main__':
+    app.run(port=4000)
